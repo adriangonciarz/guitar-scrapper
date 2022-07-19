6b280e9b178506266632e654b9e6f3dd9dea23e2
Adrian Gonciarz <adrian.gonciarz@gmail.com>
2022-07-19 11:51:23 +0200

    feat: first try of db integration

--- src/dbclient.py
+++ src/dbclient.py
@@ -14,7 +14,6 @@ sql_create_items_table_query = """CREATE TABLE IF NOT EXISTS items (
                             );"""
 
 
-
 class DBClient:
     def __init__(self, db_file):
         self.conn = None
@@ -27,10 +26,22 @@ class DBClient:
     def create_items_table(self):
         self.__execute_sql(sql_create_items_table_query)
 
-    def insert_item(self, item: Item):
+    def insert_item(self, item: Item, autocommit=True):
         hash_id = base64.b64encode(item.link.encode('ascii')).decode() if item.link else str(uuid.uuid4())
-        insert_query = "INSERT INTO items(id,link,brand,model,price) VALUES('{}','{}','{}','{}','{}');".format(hash_id, item.link, item.brand, item.model, item.price)
+        insert_query = "INSERT INTO items(id,link,brand,model,price) VALUES('{}','{}','{}','{}','{}');".format(hash_id,
+                                                                                                               item.link,
+                                                                                                               item.brand,
+                                                                                                               item.model,
+                                                                                                               item.price)
+        print(insert_query)
         self.__execute_sql(insert_query)
+        if autocommit:
+            self.__commit()
+
+    def insert_items(self, items: [Item]):
+        for item in items:
+            self.insert_item(item, autocommit=False)
+        self.__commit()
 
     def __create_connection(self, db_file):
         """ create a database connection to a SQLite database """
@@ -47,6 +58,9 @@ class DBClient:
         except Error as e:
             print(e)
 
+    def __commit(self):
+        self.conn.commit()
+
     def select_all_items_and_print(self):
         c = self.conn.cursor()
         c.execute('SELECT * FROM items')
@@ -56,9 +70,9 @@ class DBClient:
 
 if __name__ == '__main__':
     items = [
-        Item(1, 'PRS Custom 24', '2050', 'http://onet.pl/12345'),
-        Item(2, 'PRS 408', '3500', 'http://onet.pl/123454'),
+        Item(1, 'PRS Custom 24', '2050', 'http://onet.pl?item=1232345'),
+        Item(2, 'PRS 408', '3500', 'http://onet.pl?item=12a3454'),
     ]
     cli = DBClient('test_db.db')
-    cli.insert_item(items[1])
+    cli.insert_items(items)
     cli.select_all_items_and_print()

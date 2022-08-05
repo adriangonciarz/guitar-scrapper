e6a5605033cd2645b9d4dab41e12cfe277cd05fd
Adrian Gonciarz <adrian.gonciarz@gmail.com>
2022-08-05 10:09:31 +0200

    more necessary olx fixes

--- src/scrappers/olx.py
+++ src/scrappers/olx.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 from config import config
@@ -7,11 +8,18 @@ from scrappers.base import BaseScrapper
 class OLXScrapper(BaseScrapper):
     input_selector = (By.ID, 'search')
     result_container_selector = (By.XPATH, '//div[@data-testid="listing-grid"]/div[@data-cy="l-card"]')
-    title_selector = (By.CSS_SELECTOR, 'a[data-cy="listing-ad-title"]')
-    price_selector = (By.CSS_SELECTOR, 'p.price')
-    link_selector = title_selector
+    title_selector = (By.CSS_SELECTOR, 'h6')
+    price_selector = (By.XPATH, ".//p[@data-testid='ad-price']")
+    link_selector = (By.CSS_SELECTOR, 'a')
     cookies_accept_selector = (By.CSS_SELECTOR, 'button#onetrust-accept-btn-handler')
     empty_results_selector = (By.XPATH, "//p[contains(text(), 'Nie znaleźliśmy żadnych wyników')]")
+    clear_input_button_selector = (By.XPATH, '//button[@data-testid="clear-btn"]')
 
     def __init__(self):
         super().__init__(config.olx_basepath)
+
+    def clear_input(self):
+        try:
+            self.driver.find_element(*self.clear_input_button_selector).click()
+        except NoSuchElementException:
+            super().clear_input()
